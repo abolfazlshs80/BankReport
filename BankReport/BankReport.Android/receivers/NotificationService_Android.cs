5bc8d5d6885299e4d1b93fed1b00b096e0d01315
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-14 19:01:57 +0330

    ✨ feat(android): implement notification service with reply feature
    - add notification service for android with reply functionality
    - create reply receiver to handle user input from notification

---
+++ BankReport/BankReport.Android/receivers/NotificationService_Android.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using AndroidX.Core.App;
+using Xamarin.Forms;
+using MyApp.Droid;
+using BankReport.Droid.receivers;
+using BankReport.Droid;
+
+[assembly: Dependency(typeof(NotificationService_Android))]
+namespace MyApp.Droid
+{
+    public class NotificationService_Android : INotificationService
+    {
+        public void ShowMessageWithReply(string message)
+        {
+            var context = Android.App.Application.Context;
+
+            var replyLabel = "پاسخ خود را وارد کنید";
+            var remoteInput = new AndroidX.Core.App.RemoteInput.Builder("key_text_reply")
+                .SetLabel(replyLabel)
+                .Build();
+
+            var replyIntent = new Intent(context, typeof(ReplyReceiver));
+            var replyPendingIntent = PendingIntent.GetBroadcast(context, 0, replyIntent, PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Mutable);
+
+
+
+            var action = new NotificationCompat.Action.Builder(Android.Resource.Drawable.IcDialogInfo, "پاسخ دهید", replyPendingIntent)
+                .AddRemoteInput(remoteInput)
+                .Build();
+
+            var builder = new NotificationCompat.Builder(context, "default")
+                .SetContentTitle("پیام جدید")
+                .SetContentText(message)
+                .SetSmallIcon(Android.Resource.Drawable.IcDialogInfo)
+                .AddAction(action)
+                .SetAutoCancel(true);
+
+            var notificationManager = NotificationManagerCompat.From(context);
+            notificationManager.Notify(100, builder.Build());
+        }
+    }
+}
