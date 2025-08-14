575cb7bd5325394814b78a33ef138d774602e4fc
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-14 19:02:04 +0330

    ✨ feat(android): implement reply receiver for notifications
    - add broadcast receiver to handle direct replies from notifications
    - send the reply message using MessagingCenter for cross-platform handling

---
+++ BankReport/BankReport.Android/receivers/ReplyReceiver.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using AndroidX.Core.App;
+using Xamarin.Forms;
+
+namespace BankReport.Droid.receivers
+{
+
+    [BroadcastReceiver(Enabled = true)]
+    public class ReplyReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            var remoteInput = AndroidX.Core.App.RemoteInput.GetResultsFromIntent(intent);
+            if (remoteInput != null)
+            {
+                var message = remoteInput.GetCharSequence("key_text_reply");
+
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        MessagingCenter.Send<object, string>(this, "ReplyMessage", message);
+                    });
+                }
+            }
+        }
+    }
+
+
+}
