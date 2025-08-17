39e0e1cafbdaad151660516481724d8d7d311b8b
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-17 22:13:27 +0330

    ✨ feat(temp): add cloner class for temporary data storage   - introduce a static cloner class to temporarily store and retrieve data

---
+++ BankReport/BankReport/Models/Temp/Cloner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankReport.Models.Temp
+{
+    public static class Cloner
+    {
+        public static object Value { get; set; }
+    }
+}
