16de04606df9b3497d9f0f497665bc9ba4728fd5
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-17 22:14:26 +0330

    ♻️ refactor(android): update android sms permission handling
    - migrate to androidX
    - add foreground service
    - improve permission requests

--- BankReport/BankReport.Android/MainActivity.cs
+++ BankReport/BankReport.Android/MainActivity.cs
@@ -3,101 +3,59 @@ using Android.App;
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using Android.Widget;
-using BankReport.Droid.receivers;
-using Plugin.LocalNotification;
-using System;
+using AndroidX.Core.App;
 
 namespace BankReport.Droid
 {
-    [Activity(Label = "BankReport", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "BankReport", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-
         private const int RequestSmsPermissionId = 100;
-        // private SmsBroadcastReceiver _smsBroadcastReceiver;
-        void CreateNotificationChannel()
-        {
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                var channel = new NotificationChannel(
-                    "default",        // همون ID که تو Builder استفاده کردی
-                    "Default Channel",
-                    NotificationImportance.High
-                )
-                {
-                    Description = "Channel for app notifications"
-                };
-
-                var notificationManager = (NotificationManager)GetSystemService(NotificationService);
-                notificationManager.CreateNotificationChannel(channel);
-            }
-        }
-
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            //Configuration config = Resources.Configuration;
-            //config.FontScale = 1.0f; // جلوگیری از تغییر اندازه فونت
-            //CreateConfigurationContext(config);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
 
             RequestSmsPermissions();
-            IntentFilter filter = new IntentFilter("SMS_RECEIVED_ACTION");
 
-            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
-            FFImageLoading.Svg.Forms.SvgCachedImage.Init();
-
-            CreateNotificationChannel();
-            var intent = new Intent(this, typeof(MyForegroundService));
-            StartForegroundService(intent);
-            NotificationCenter.CreateNotificationChannel();
-        }
-
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-
-            // پاک کردن داده‌ها
-            Xamarin.Essentials.Preferences.Clear();
-            // حذف BroadcastReceiver
-            //if (_smsBroadcastReceiver != null)
-            //{
-            //    UnregisterReceiver(_smsBroadcastReceiver);
-            //}
+            // استارت Foreground Service
+            var intent = new Intent(this, typeof(Services.MyForegroundService));
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+                StartForegroundService(intent);
+            else
+                StartService(intent);
         }
 
         private void RequestSmsPermissions()
         {
-            // بررسی نسخه اندروید (تنها برای نسخه‌های 6.0 به بالا نیاز است)
             if ((int)Build.VERSION.SdkInt >= 23)
             {
-                // بررسی آیا مجوز داده شده یا نه
                 if (CheckSelfPermission(Manifest.Permission.ReceiveSms) != Permission.Granted ||
-                CheckSelfPermission(Manifest.Permission.SendSms) != Permission.Granted ||
-                CheckSelfPermission(Manifest.Permission.ReadPhoneState) != Permission.Granted ||
-                CheckSelfPermission(Manifest.Permission.ReadPhoneNumbers) != Permission.Granted ||
-                    CheckSelfPermission(Manifest.Permission.ReadSms) != Permission.Granted)
+                    CheckSelfPermission(Manifest.Permission.ReadSms) != Permission.Granted ||
+                    CheckSelfPermission(Manifest.Permission.ReadPhoneState) != Permission.Granted ||
+                    CheckSelfPermission(Manifest.Permission.ReadPhoneNumbers) != Permission.Granted ||
+                    CheckSelfPermission(Manifest.Permission.SendSms) != Permission.Granted ||
+                    CheckSelfPermission(Manifest.Permission.PostNotifications) != Permission.Granted)
                 {
-                    // درخواست مجوز
                     RequestPermissions(new string[]
                     {
-                    Manifest.Permission.ReceiveSms,
-                    Manifest.Permission.ReadSms,
-                    Manifest.Permission.ReadPhoneState,
-                    Manifest.Permission.ReadPhoneNumbers,
-                    Manifest.Permission.SendSms
+                        Manifest.Permission.ReceiveSms,
+                        Manifest.Permission.ReadSms,
+                        Manifest.Permission.ReadPhoneState,
+                        Manifest.Permission.ReadPhoneNumbers,
+                        Manifest.Permission.SendSms,
+                        Manifest.Permission.PostNotifications
                     }, RequestSmsPermissionId);
                 }
             }
         }
 
-        // این متد نتیجه درخواست مجوز را دریافت می‌کند
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -105,18 +63,10 @@ namespace BankReport.Droid
             if (requestCode == RequestSmsPermissionId)
             {
                 if (grantResults.Length > 0 && grantResults[0] == Permission.Granted)
-                {
-                    // مجوزها تایید شد
                     Toast.MakeText(this, "مجوز خواندن SMS داده شد.", ToastLength.Short).Show();
-                }
                 else
-                {
-                    // مجوز رد شد
                     Toast.MakeText(this, "مجوز خواندن SMS رد شد.", ToastLength.Short).Show();
-                }
             }
-
         }
-
     }
 }
