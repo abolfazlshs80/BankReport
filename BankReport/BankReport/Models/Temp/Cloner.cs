83e9af6d9d83f40af432fee6a1e139a6854c3e91
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-08-29 10:49:07 +0330

    ✨ feat(cloner): add messages dictionary to cloner
    - add Messages dictionary to store messages with integer keys
    - create ClonerNottificationId class to manage notification IDs

--- BankReport/BankReport/Models/Temp/Cloner.cs
+++ BankReport/BankReport/Models/Temp/Cloner.cs
@@ -7,5 +7,13 @@ namespace BankReport.Models.Temp
     public static class Cloner
     {
         public static object Value { get; set; }
+        public static Dictionary<int, object> Messages { get; set; } = new Dictionary<int, object>();
     }
+    public static class ClonerNottificationId
+    {
+        public static int Value { get; set; }
+    }
+
+    
+
 }
